4fb15ade09e8a6c8cfe303647afea972a0612285
qaraluch <prezenciq@gmail.com>
2017-09-19 07:35:16 +0200

    Add codition for oh-my-zsh installation

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ _C_END=$'\033[0m'
 _PROMPT="[ dotfiles ]"
 _TASK="[ install ]"
 _TICK="[ ${_C_GREEN}✔${_C_END} ]"
+_CROSS="[ ${_C_YELLOW}✖${_C_END} ]"
 _empty_line() {
   echo ' '
 }
@@ -18,7 +19,7 @@ echo "--------------------------------------------------------------------------
 
 ######################################################################### UPDATES ######
 git pull
-echo "${_PROMPT}${_TASK}${_TICK} Dotfiles are uptodate"
+echo "${_PROMPT}${_TASK}${_TICK} Dotfiles are up-to-dated!"
 
 echo "${_PROMPT}${_TASK} About to run sys updates..."
 sudo apt -qq update
@@ -30,16 +31,19 @@ echo "${_PROMPT}${_TASK}${_TICK} Done updatings!"
 sudo apt -qq install zsh
 echo "${_PROMPT}${_TASK}${_TICK} Installed zsh"
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "${_PROMPT}${_TASK}${_TICK} Installed oh-my-zsh"
+if [ ! -d $HOME/.oh-my-zsh  ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "${_PROMPT}${_TASK}${_TICK} Installed oh-my-zsh"
+else
+  echo "${_PROMPT}${_TASK}${_CROSS} Skiped installation of oh-my-zsh. Already installed!"
+fi
 
-sudo apt -qq install dos2unix unix2dos
-echo "${_PROMPT}${_TASK}${_TICK} Installed dos2unix unix2dos"
+sudo apt -qq install dos2unix
+echo "${_PROMPT}${_TASK}${_TICK} Installed dos2unix"
 
 
 # sudo apt -qq install zsh
 # echo "${_PROMPT}${_TASK}${_TICK} Installed zsh"
-
 
 ######################################################################### FOOTER #######
 echo "-------------------------------------------------------------------- [ END ] ----"
